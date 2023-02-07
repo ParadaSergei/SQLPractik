5d45b847d89236426fe36b04b2f3445d04168b71
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2023-02-07 13:04:00 +0300

    Add files via upload

---
+++ SQLQuery71.sql
@@ -0,0 +1,36 @@
+USE PostavkiTovar
+GO
+
+--1
+SELECT S.Sname 
+FROM S
+WHERE S.Status BETWEEN 50 AND 100
+
+--2
+SELECT COUNT(S.Snum) KOLVO
+
+FROM S
+WHERE S.City = 'Москва'
+
+--3
+SELECT DISTINCT SUM(STD.Kolvo) AS obshie
+FROM STD,D,T
+WHERE (STD.Tnum = T.Tnum) AND (D.Dnum = 'D1')AND (STD.Dnum = D.Dnum)
+
+--4
+SELECT S.Sname, AVG(T.Price*STD.Kolvo) AS Srednie  
+FROM S,T,STD
+WHERE (STD.Tnum = T.Tnum)AND (STD.Snum = S.Snum)
+GROUP BY Sname
+
+
+
+
+--5
+SELECT S.City
+FROM S ,D , STD
+WHERE STD.Snum = S.City
+
+
+
+
