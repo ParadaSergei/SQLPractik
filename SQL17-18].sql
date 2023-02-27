5dd41ca3e8b12215aedd80da9691237d26dab3c0
Парада Сергей Сергеевич <106469820+ParadaSergei@users.noreply.github.com>
2023-02-27 17:35:27 +0300

    Add files via upload

--- SQL17-18].sql
+++ SQL17-18].sql
@@ -1,85 +1,76 @@
---1
+--1 
 
---CREATE PROC [INSERT]
+--CREATE PROC [InsertNewLecturer]
 --@name nvarchar(50) ,
 --@middlename nvarchar(50),
 --@lastName nvarchar(50) = null,
 --@description nvarchar(100)
---as 
---begin
---insert into [Lecturer] ([Name] , [MiddleName] , [LastName] ,[Description])
+--AS 
+--BEGIN
+--INSERT INTO [Lecturer] ([Name] , [MiddleName] , [LastName] ,[Description])
 --VALUES (@name , @middlename , @lastName ,@description);
---end;
---go
---exec [INSERT] @name = 'Александр' ,@middlename='Козбеков' , @lastName='Петрович' , @description ='Учитель математики'
+--END;
+----GO
+--CREATE PROC [InsertNewLesson]
+--@name nvarchar(50) ,
+--@description nvarchar(100)
+--AS 
+--BEGIN
+--INSERT INTO [LessonObject] ([Name] , [Description])
+--VALUES (@name ,@description);
+--END;
+--GO
+--EXEC [InsertNewLecturer] @name = 'Александр' ,@middlename='Козбеков' , @lastName='Петрович' , @description ='Учитель математики'
+--EXEC [InsertNewLesson] @name = 'Александр', @description ='Учитель математики'
 
 
 
---2
+--2 
 --Напишите функцию получения Id преподавателя и функцию получения Id предмета;
 
 
 
---CREATE FUNCTION [Lecturer] (@name nvarchar(50), @middleName nvarchar(50))
---returns int as 
---begin
---	declare @id int = (SELECT Id
+--CREATE FUNCTION [GetLecturerId] (@name nvarchar(50), @middleName nvarchar(50))
+--RETURNS INT AS 
+--BEGIN
+--	DECLARE @id INT = (SELECT Id
 --	FROM Lecturer
 --	WHERE (Name = @name)AND (MiddleName = @middleName))
---	return @id;
---end;
---go
+--	RETURN @id;
+--END;
+--GO
 
---CREATE FUNCTION [Lesson] (@name nvarchar(50), @middleName nvarchar(50))
---returns int as 
---begin
---	declare @lessonid int = (SELECT LessonObjectId
---	FROM LessonsAtDay,Lecturer
+--CREATE FUNCTION [GetLessonId] (@name nvarchar(50), @middleName nvarchar(50))
+--RETURNS int AS 
+--BEGIN
+--	DECLARE @lessonid INT = (SELECT LessonObjectId
+--	FROM LessonObject,Lecturer
 --	WHERE (Name = @name)AND (MiddleName = @middleName))
---	return @lessonid;
---end;
---go
+--	RETURN @lessonid;
+--END;
+--GO
 
 
 
---3
+--3 
 --Напишите процедуру удаления предмета и процедуру удаления преподавателя;
 
---CREATE PROC [Delete]
---@lesson nvarchar(50),
+--CREATE PROC [DeleteLesson]
+--@lesson nvarchar(50)
+--AS
+--BEGIN
+--DELETE FROM [LessonObject] WHERE [LessonObject].[LecturerId] = @lesson 
+--END;
+--GO
+
+--CREATE PROC [DeleteLecturer]
 --@lecturer  nvarchar(50)
---as
---begin
---delete from [LessonObject] where [LessonObject].[Name] = @lesson and [LessonObject].[LecturerId] = @lecturer
---end;
---go
+--AS
+--BEGIN
+--DELETE FROM [LessonObject] WHERE [LessonObject].[LecturerId] = @lecturer
+--END;
+--GO
+
+EXEC [DeleteLesson] @lesson = '1'
 
 --4
-
-
-create function [GetDayIdByName](@day nvarchar(255))
-returns int as 
-begin
-    declare @dayId int = (select max(D.[Id]) 
-                          from DayType DT join [Day] as D 
-                          on DT.[Type] = lower(@day))
-    return @dayId;
-end;
-go
-
-
-create proc [UpdateLessonNumber]
-    @day        nvarchar (255),
-    @weekId     int, 
-    @oldNumber  int,
-    @newNumber  int
-as
-begin
-    declare @dayId int = dbo.[GetDayIdByName](@day);
-    update [LessonsAtDay] set [Number] = @newNumber 
-    where [LessonsAtDay].DayId = @dayId and 
-          LessonsAtDay.WeekId = @weekId and 
-          LessonsAtDay.Number = @oldNumber;  
-end;
-go
-
